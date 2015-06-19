3fc6f7d6d0ebc2dd65274208517b2c9ca220eb21
Jens Renken <renken@marum.de>
2015-06-19 12:38:03 +0200

    workaround for import of custom widgets on windows installation

---
+++ gui/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+if os.name == 'nt':
+	''' Workaround for windows platform.
+		Currently the qgis_customwidgets plugin is moved from 
+		the directory where it is searched to another directory.
+		I don't know why
+	'''
+	import qgis
+	from PyQt4 import uic
+
+
+	uic.widgetPluginPath.append( os.path.normpath(
+		os.path.join(qgis.__path__[0], '..', 'PyQt4', 'uic', 'widget-plugins')))
+
