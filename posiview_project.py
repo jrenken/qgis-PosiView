2e32060122f99e1ea41a8d239ae430dfdd665416
Jens Renken <renken@marum.de>
2015-12-18 14:16:54 +0100

    define type for QSettings.value

--- posiview_project.py
+++ posiview_project.py
@@ -150,7 +150,7 @@ class PosiViewProject(object):
         properties['Mission']['dive'] = s.value('Mission/Dive', 'DiveX')
         properties['Mission']['station'] = s.value('Mission/Station', '#xxx')
         properties['RecorderPath'] = s.value('Recorder/Path', environ['HOME'])
-        properties['AutoRecord'] = s.value('Recorder/AutoRecord', False) in ['True', 'true']
+        properties['AutoRecord'] = s.value('Recorder/AutoRecord', False, type=bool)
         s.endGroup()
         return properties
 
