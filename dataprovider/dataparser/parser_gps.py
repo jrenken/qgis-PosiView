43987dbb957e3e0b80b0ef3a71c633eb8d1e8e89
Jens Renken <renken@marum.de>
2016-05-12 10:12:43 +0200

    parse GPGGA in GPS parser

--- dataprovider/dataparser/parser_gps.py
+++ dataprovider/dataparser/parser_gps.py
@@ -13,6 +13,7 @@ class GpsParser(Parser):
     '''
     $GPRMC,140808,A,5301.4970,N,00852.1740,E,000.0,000.0,030715,0.4,E,N*15
     $GPGLL,3836.1382,N,01712.6234,E,185000.00,A,D*60
+    $GPGGA,075222.00,3727.35636,N,01509.01712,E,1,07,1.8,17.35,M,40.17,M,,*5C
     '''
 
     def __init__(self):
@@ -28,6 +29,8 @@ class GpsParser(Parser):
             return self.decodeVtg(data)
         elif data.startswith('$GPGLL'):
             return self.decodeGll(data)
+        elif data.startswith('$GPGGA'):
+            return self.decodeGga(data)
 
     def decodeRmc(self, data):
         nmea = NmeaRecord(data)
@@ -53,9 +56,26 @@ class GpsParser(Parser):
                 result = {'lat': nmea.fromDDM(1, 2),
                           'lon': nmea.fromDDM(3, 4)}
                 try:
-                    dt = datetime.datetime(int(nmea[9][4:]) + 2000, int(nmea[9][2:4]), int(nmea[9][0:2]),
-                                   int(nmea[1][0:2]), int(nmea[1][2:4]),
-                                   int(nmea[1][4:6]))
+                    dt = datetime.datetime.utcnow().replace(hour=int(nmea[1][0:2]),
+                                        minute=int(nmea[1][2:4]), second=int(nmea[1][4:6]))
+                except ValueError:
+                    dt = datetime.datetime.utcnow()
+                td = dt - datetime.datetime(1970, 1, 1)
+                result['time'] = td.total_seconds()
+                return dict((k, v) for k, v in result.iteritems() if v is not None)
+            except ValueError:
+                return {}
+
+    def decodeGga(self, data):
+        nmea = NmeaRecord(data)
+        if nmea.valid:
+            try:
+                result = {'lat': nmea.fromDDM(2, 3),
+                          'lon': nmea.fromDDM(4, 5),
+                          'depth': -float(nmea[9])}
+                try:
+                    dt = datetime.datetime.utcnow().replace(hour=int(nmea[1][0:2]),
+                                        minute=int(nmea[1][2:4]), second=int(nmea[1][4:6]))
                 except ValueError:
                     dt = datetime.datetime.utcnow()
                 td = dt - datetime.datetime(1970, 1, 1)
