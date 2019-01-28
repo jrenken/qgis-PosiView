5e968dd5343bdeba8d96bebc1a0bed9dedb6e6ae
Jens Renken <renken@marum.de>
2019-01-28 14:15:55 +0100

    demonstrate how to create a custom parser

---
+++ dataprovider/dataparser/parser_test.py
@@ -0,0 +1,28 @@
+'''
+Created on Jan 28, 2019
+
+@author: jrenken
+'''
+
+from .parser import Parser
+from datetime import datetime
+
+class TestParser(Parser):
+    '''
+    Sentence parser for custom protocol
+    
+    '''
+
+    def __init__(self):
+        super(TestParser, self).__init__()
+
+    def parse(self, data):
+        try:
+            fields = data.split(',')
+            dt = datetime.utcnow()
+            
+            return { 'id':fields[0], 'lat': float(fields[1]), 'lon':float(fields[2]),
+                     'time': (dt - datetime(1970,1,1,0,0,0)).total_seconds() }
+        except ValueError:
+            return {}
+        
