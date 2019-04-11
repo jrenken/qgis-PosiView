74705b9dcf70d3f1de17d69f52575b95d933a7a6
Jens Renken <renken@marum.de>
2019-04-11 10:09:13 +0200

    try to avoid QVariant error on plugin loading

--- posi_view.py
+++ posi_view.py
@@ -57,7 +57,7 @@ class PosiView(object):
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
         # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
+        locale = QSettings().value('locale/userLocale', 'en_US')[0:2]
         locale_path = os.path.join(
             self.plugin_dir,
             'i18n',
