594aef846598124b91dd6915ea9795ba4aefb23c
Jens Renken <renken@marum.de>
2020-03-02 12:50:29 +0100

    remove altitude from P16 parser

--- dataprovider/dataparser/parser_cp16.py
+++ dataprovider/dataparser/parser_cp16.py
@@ -26,7 +26,6 @@ class CP16Parser(Parser):
             if nmea.valid:
                 try:
                     result = {'depth': nmea.value(1),
-                              'altitude': nmea.value(2),
                               'heading': nmea.value(3),
                               'pitch': nmea.value(5),
                               'roll': nmea.value(6)}
